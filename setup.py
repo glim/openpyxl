a35a4ad819cbe6ec4a47371cffda532ab1faabdf
jasdf <jameson.holden@gmail.com>
2013-02-13 12:02:02 -0500

    moving to setup package
    trying to allow installing from github

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, find_packages
+
+setup(name='openpyxl',
+      version='1.6.1',
+      description='Python bindings for MS Excel',
+      author='Eric Gazoni',
+      author_email='unkown@unkonwn.net',
+      url='https://github.com/glim/openpyxl',
+      packages=find_packages(),
+     )
